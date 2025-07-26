4516a2088180af6ad018524093e2669e9d8ce81d
Jacob Johnstone <jjohn483@uwo.ca>
2025-07-26 16:46:34 -0400

    Finished scene changes up until level 2, Fixed level 2 ground layer

---
+++ GameJam2/Assets/Spawner2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Spawner2 : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public Transform enterSpawn;
+    public Transform winCombatSpawn;
+    public Transform returnSpawn;
+
+    void Start()
+    {
+        DoorIDs targetID = SceneSpawns.instance.doorID;
+
+        if (targetID == DoorIDs.enterLevel1 || targetID == DoorIDs.enterLevel2 || targetID == DoorIDs.enterLevel3)
+        {
+            Instantiate(playerPrefab, enterSpawn);
+        }
+        else if (targetID == DoorIDs.returnLevel1 || targetID == DoorIDs.returnLevel2)
+        {
+            Instantiate(playerPrefab, returnSpawn);
+        } else
+        {
+            Instantiate(playerPrefab, winCombatSpawn);
+        }
+    }
+}
